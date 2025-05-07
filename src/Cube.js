7b2fa24ee6e9372ae38a6fbb4d62c1aa528d5cf2
Akshay Kamath <akkamath@ucsc.edu>
2025-05-06 18:28:03 -0700

    update until video 4 of series

--- src/Cube.js
+++ src/Cube.js
@@ -10,8 +10,8 @@ class Cube{
     
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
-        drawTriangle3D([0.0, 0.0, 0.0,   1.0, 1.0, 0.0,  1.0, 0.0, 0.0]);
-        drawTriangle3D([0.0, 0.0, 0.0,   0.0, 1.0, 0.0,  1.0, 1.0, 0.0]);
+        drawTriangle3DUV([0.0, 0.0, 0.0,   1.0, 1.0, 0.0,  1.0, 0.0, 0.0], [0, 0, 1, 1, 1, 0]);
+        drawTriangle3D([0.0, 0.0, 0.0,   0.0, 1.0, 0.0,  1.0, 1.0, 0.0], [0, 0, 0, 1, 1, 1]);
     
         gl.uniform4f(u_FragColor, rgba[0] * 0.9, rgba[1] * 0.9, rgba[2] * 0.9, rgba[3]);
         drawTriangle3D([0.0, 0.0, 1.0,   1.0, 1.0, 1.0,  1.0, 0.0, 1.0]);
